9eb549bfcdf4e6814cf00044cba097b324646c31
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-21 17:44:52 +0000

    updated files

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 
 # Create a Flask instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "My secret key for movies buddy"
-
-selected_genre = []
 
 # Create a Form Class for homepage
 
@@ -15,19 +13,31 @@ class ButtonForm(FlaskForm):
     submit = SubmitField("Find a movie")
 
 # Create a Form Class for input
-class OptionsForm(FlaskForm):
-    submit = SubmitField("Find a movie")
+class StringForm(FlaskForm):
+    s = 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = ButtonForm()
     return render_template('index.html', form=form)
 
-@app.route('/input', methods=['GET', 'POST'])
-def user_input():
-    form = OptionsForm()
-    return render_template('input.html', form=form)
+selected = []
+@app.route('/options', methods=['GET', 'POST'])
 
+def options():
+    # return request.method 
+    if request.method == "POST":
+        global selected
+        selected = request.form.getlist('options')
+
+        if len(selected) > 0:
+            return redirect(url_for('input1'))
+    return render_template('options.html')
+
+@app.route('/input', methods=['GET', 'POST'])
+def input1():
+    print(selected)
+    return render_template('input.html', selected=selected)
 
 
 if __name__ == '__main__':               # this should always be at the end
